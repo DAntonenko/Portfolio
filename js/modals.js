df1772fa2b34651aad7a1ab9d740a2bd30262d0b
Danton <dm.antonenko1977@gmail.com>
2018-10-08 04:14:21 +0300

    Добавлены попапы проектов
    Пока не всё работает как надо, но базовый функционал попапов есть.

--- js/modals.js
+++ js/modals.js
@@ -4,6 +4,19 @@ var sideMenu = document.querySelector(".side-menu");
 
 var hamburgerLineUpper = document.querySelector(".hamburger-line_upper");
 
+var projectCards = document.querySelectorAll(".project-card");
+var popups = document.querySelectorAll(".popup");
+
+var projectCardBarbersop = document.getElementById("barbershop");
+var projectCardSedona = document.getElementById("sedona");
+var projectCardHomeownersAssociation = document.getElementById("homeowners-association");
+var projectCardCertificate = document.getElementById("certificate");
+
+var popupBarbersop = document.querySelector(".popup-barbershop");
+var popupSedona = document.querySelector(".popup-sedona");
+var popupHomeownersAssociation = document.querySelector(".popup-homeowners_association");
+var popupCertificate = document.querySelector(".popup-certificate");
+
 hamburger.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	curtain.classList.toggle("modal-hide");
@@ -34,15 +47,46 @@ hamburger.addEventListener("click", function(evt) {
 	}
 });
 
-/*
-window.addEventListener("keydown", function (evt) {
+/* escape events */
+
+window.addEventListener("keydown", function(evt) {
 	if (evt.keyCode === 27) {
 		evt.preventDefault();
+		curtain.classList.add("modal-hide");
+		sideMenu.classList.add("modal-hide");
 
-		if (!curtain.classList.contains("modal-hide")) {
-			curtain.classList.add("modal-hide");
-			sideMenu.classList.add("modal-hide");
-		}
+		popups.forEach(function(elem) {
+    elem.classList.remove("popup-show");
+		});
 	}
 });
-*/
+
+/* popup events */
+
+projectCards.forEach(function(elem) {
+	elem.addEventListener("click", function(evt) {
+		evt.preventDefault();
+		curtain.classList.remove("modal-hide");
+	});
+});
+
+projectCardBarbersop.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popupBarbersop.classList.add("popup-show");
+});
+
+projectCardSedona.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popupSedona.classList.add("popup-show");
+});
+
+projectCardHomeownersAssociation.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popupHomeownersAssociation.classList.add("popup-show");
+});
+
+projectCardCertificate.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popupCertificate.classList.add("popup-show");
+});
+
