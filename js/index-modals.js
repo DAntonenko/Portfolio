2444d85283591ea74876a0823cc14676b41d301d
Danton <dm.antonenko1977@gmail.com>
2018-10-13 19:03:25 +0300

    Добавлена страница с фотогалереей

--- js/index-modals.js
+++ js/index-modals.js
@@ -2,8 +2,6 @@ var hamburger = document.querySelector(".hamburger");
 var curtain = document.querySelector(".curtain");
 var sideMenu = document.querySelector(".side-menu");
 
-var hamburgerLineUpper = document.querySelector(".hamburger-line_upper");
-
 var projectCards = document.querySelectorAll(".project-card");
 var popups = document.querySelectorAll(".popup");
 
@@ -11,19 +9,53 @@ var projectCardBarbersop = document.getElementById("barbershop");
 var projectCardSedona = document.getElementById("sedona");
 var projectCardHomeownersAssociation = document.getElementById("homeowners-association");
 var projectCardCertificate = document.getElementById("certificate");
+var projectCardThisSite = document.getElementById("this-site");
 
 var popupBarbersop = document.querySelector(".popup-barbershop");
 var popupSedona = document.querySelector(".popup-sedona");
 var popupHomeownersAssociation = document.querySelector(".popup-homeowners_association");
 var popupCertificate = document.querySelector(".popup-certificate");
+var popupThisSite = document.querySelector(".popup-this-site");
+
+var previewUniver = document.getElementById("preview-univer");
+var previewWinners = document.getElementById("preview-winners");
+var previewPotter = document.getElementById("preview-potter");
+var previewLecturer = document.getElementById("preview-lecturer");
+var previewYahtFront = document.getElementById("preview-yaht_front");
+var previewYahtSideways = document.getElementById("preview-yaht_sideways");
+var previewMermaid = document.getElementById("preview-mermaid");
+var previewViruRAba = document.getElementById("preview-viru_raba");
+var previewSaareTirp = document.getElementById("preview-saare_tirp");
+
+var photoUniver = document.getElementById("photo-univer");
+var photoWinners = document.getElementById("photo-winners");
+var photoPotter = document.getElementById("photo-potter");
+var photoLecturer = document.getElementById("photo-lecturer");
+var photoYahtFront = document.getElementById("photo-yaht_front");
+var photoYahtSideways = document.getElementById("photo-yaht_sideways");
+var photoMermaid = document.getElementById("photo-mermaid");
+var photoViruRAba = document.getElementById("photo-viru_raba");
+var photoSaareTirp = document.getElementById("photo-saare_tirp");
 
 hamburger.addEventListener("click", function(evt) {
 	evt.preventDefault();
-	curtain.classList.toggle("modal-hide");
 	sideMenu.classList.toggle("modal-hide");
 
-	if (curtain.classList.contains("curtain-close")) {
-		curtain.classList.add("curtain-open");
+	if (curtain.classList.contains("modal-show")) {
+		curtain.classList.remove("modal-show");
+		curtain.classList.add("modal-hide");
+	} 
+	else {
+		curtain.classList.add("modal-show");
+		curtain.classList.remove("modal-hide");
+	};
+
+	popups.forEach(function(elem) {
+    elem.classList.remove("popup-show");
+		});
+/*
+	if (curtain.classList.contains("modal-show")) {
+		curtain.classList.add("modal-hide");
 		curtain.classList.remove("curtain-close");
 		curtain.classList.remove("modal-show");
 	} 
@@ -32,8 +64,7 @@ hamburger.addEventListener("click", function(evt) {
 		curtain.classList.remove("curtain-open");
 		curtain.classList.add("modal-show");
 		hamburgerLineUpper.classList.add("line-down");
-		/*hamburgerLineUpper.classList.add("line-rotate");*/
-	}
+	};
 
 	if (sideMenu.classList.contains("curtain-close")) {
 		sideMenu.classList.add("curtain-open");
@@ -44,7 +75,8 @@ hamburger.addEventListener("click", function(evt) {
 		sideMenu.classList.add("curtain-close");
 		sideMenu.classList.remove("curtain-open");
 		sideMenu.classList.add("modal-show");
-	}
+	};
+	*/
 });
 
 /* escape events */
@@ -52,6 +84,7 @@ hamburger.addEventListener("click", function(evt) {
 window.addEventListener("keydown", function(evt) {
 	if (evt.keyCode === 27) {
 		evt.preventDefault();
+		curtain.classList.remove("modal-show");
 		curtain.classList.add("modal-hide");
 		sideMenu.classList.add("modal-hide");
 
@@ -59,6 +92,19 @@ window.addEventListener("keydown", function(evt) {
     elem.classList.remove("popup-show");
 		});
 	}
+});
+
+/* curtain click events */
+
+curtain.addEventListener("click", function(evt) {
+		evt.preventDefault();
+		curtain.classList.remove("modal-show");
+		curtain.classList.add("modal-hide");
+		sideMenu.classList.add("modal-hide");
+
+		popups.forEach(function(elem) {
+    elem.classList.remove("popup-show");
+  	});
 });
 
 /* popup events */
@@ -90,3 +136,15 @@ projectCardCertificate.addEventListener("click", function(evt) {
 	popupCertificate.classList.add("popup-show");
 });
 
+projectCardThisSite.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popupThisSite.classList.add("popup-show");
+});
+
+/* photo gallery events */
+
+previewUniver.addEventListener("click", function(evt) {
+	console.log("jah!")
+	evt.preventDefault();
+	photoUniver.classList.remove("modal-hide");
+});
---
+++ js/personal-modals.js
@@ -0,0 +1,146 @@
+var hamburger = document.querySelector(".hamburger");
+var curtain = document.querySelector(".curtain");
+var sideMenu = document.querySelector(".side-menu");
+
+var previews = document.querySelectorAll(".photo-preview");
+var photoCanvases = document.querySelectorAll(".photo-canvas");
+
+var previewUniver = document.getElementById("preview-univer");
+var previewWinners = document.getElementById("preview-winners");
+var previewPotter = document.getElementById("preview-potter");
+var previewLecturer = document.getElementById("preview-lecturer");
+var previewYahtFront = document.getElementById("preview-yaht_front");
+var previewYahtSideways = document.getElementById("preview-yaht_sideways");
+var previewMermaid = document.getElementById("preview-mermaid");
+var previewViruRAba = document.getElementById("preview-viru_raba");
+var previewSaareTirp = document.getElementById("preview-saare_tirp");
+
+var photoUniver = document.getElementById("photo-univer");
+var photoWinners = document.getElementById("photo-winners");
+var photoPotter = document.getElementById("photo-potter");
+var photoLecturer = document.getElementById("photo-lecturer");
+var photoYahtFront = document.getElementById("photo-yaht_front");
+var photoYahtSideways = document.getElementById("photo-yaht_sideways");
+var photoMermaid = document.getElementById("photo-mermaid");
+var photoViruRAba = document.getElementById("photo-viru_raba");
+var photoSaareTirp = document.getElementById("photo-saare_tirp");
+
+hamburger.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	sideMenu.classList.toggle("modal-hide");
+
+	if (curtain.classList.contains("modal-show")) {
+		curtain.classList.remove("modal-show");
+		curtain.classList.add("modal-hide");
+	} 
+	else {
+		curtain.classList.add("modal-show");
+		curtain.classList.remove("modal-hide");
+	};
+
+	popups.forEach(function(elem) {
+    elem.classList.remove("popup-show");
+		});
+/*
+	if (curtain.classList.contains("modal-show")) {
+		curtain.classList.add("modal-hide");
+		curtain.classList.remove("curtain-close");
+		curtain.classList.remove("modal-show");
+	} 
+	else {
+		curtain.classList.add("curtain-close");
+		curtain.classList.remove("curtain-open");
+		curtain.classList.add("modal-show");
+		hamburgerLineUpper.classList.add("line-down");
+	};
+
+	if (sideMenu.classList.contains("curtain-close")) {
+		sideMenu.classList.add("curtain-open");
+		sideMenu.classList.remove("curtain-close");
+		sideMenu.classList.remove("modal-show");
+	} 
+	else {
+		sideMenu.classList.add("curtain-close");
+		sideMenu.classList.remove("curtain-open");
+		sideMenu.classList.add("modal-show");
+	};
+	*/
+});
+
+/* escape events */
+
+window.addEventListener("keydown", function(evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		curtain.classList.remove("modal-show");
+		curtain.classList.add("modal-hide");
+		sideMenu.classList.add("modal-hide");
+
+		photoCanvases.forEach(function(elem) {
+	    elem.classList.add("modal-hide");
+		});
+	}
+});
+
+/* curtain click events */
+
+curtain.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	curtain.classList.remove("modal-show");
+	curtain.classList.add("modal-hide");
+	sideMenu.classList.add("modal-hide");
+});
+
+/* photo gallery events */
+
+photoCanvases.forEach(function(elem) {
+	elem.addEventListener("click", function(evt) {
+		evt.preventDefault();
+		elem.classList.add("modal-hide");
+	});
+});
+
+previewUniver.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoUniver.classList.remove("modal-hide");
+});
+
+previewWinners.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoWinners.classList.remove("modal-hide");
+});
+
+previewPotter.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoPotter.classList.remove("modal-hide");
+});
+
+previewLecturer.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoLecturer.classList.remove("modal-hide");
+});
+
+previewYahtFront.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoYahtFront.classList.remove("modal-hide");
+});
+
+previewYahtSideways.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoYahtSideways.classList.remove("modal-hide");
+});
+
+previewMermaid.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoMermaid.classList.remove("modal-hide");
+});
+
+previewViruRAba.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoViruRAba.classList.remove("modal-hide");
+});
+
+previewSaareTirp.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	photoSaareTirp.classList.remove("modal-hide");
+});
